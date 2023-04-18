a4c718fef40cdb5144d5944dd0b008fa9a70d650
Ernest728 <41069663+Ernest728@users.noreply.github.com>
2023-04-19 02:39:46 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -31,37 +31,4 @@ ax[2].plot(t,z)
 ax[2].set_xlabel('Time')
 ax[2].set_ylabel('Z-axis')
 plt.show()
-s.close()import numpy as np
-import serial
-import matplotlib.pyplot as plt
-import time
-serdev = 'COM4'
-s = serial.Serial(serdev)
-x = []
-y = []
-z = []
-t = np.arange(0,1.27,0.01)
-s.write(b'1')
-time.sleep(10)
-for i in range(0, 127):
-    x.append(float(s.readline().decode()))
-    y.append(float(s.readline().decode()))
-    z.append(float(s.readline().decode()))
-fig, ax = plt.subplots(3, 1)
-y_ticks = np.linspace(min(x), max(x), 10)
-plt.yticks(y_ticks)
-ax[0].plot(t,x)
-ax[0].set_xlabel('Time')
-ax[0].set_ylabel('X-axis')
-y_ticks = np.linspace(min(y), max(y), 10)
-plt.yticks(y_ticks)
-ax[1].plot(t,y)
-ax[1].set_xlabel('Time')
-ax[1].set_ylabel('Y-axis')
-y_ticks = np.linspace(min(z), max(z), 10)
-plt.yticks(y_ticks)
-ax[2].plot(t,z)
-ax[2].set_xlabel('Time')
-ax[2].set_ylabel('Z-axis')
-plt.show()
 s.close()
